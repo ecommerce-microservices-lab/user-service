70174f9ec748a9f95fc2be3ff4bf46bb31063a71
santiesleo <santiago.escobar3@u.icesi.edu.co>
2025-10-31 07:38:04 -0500

    chore(ci): enhance workflows with additional secrets and update GitHub token

--- src/main/resources/db/migration/V6__insert_credentials_table.sql
+++ src/main/resources/db/migration/V6__insert_credentials_table.sql
@@ -2,4 +2,4 @@ INSERT INTO credentials (user_id, username, password, role, is_enabled) VALUES
 (1, 'selimhorri', '$2a$04$6B5YIsWvW4CkdVgE/9IIyONxabx4mwjmqmJhWZQr0/nWz2.r.43mO', 'ROLE_USER', TRUE),
 (2, 'amineladjimi', '$2a$04$6B5YIsWvW4CkdVgE/9IIyONxabx4mwjmqmJhWZQr0/nWz2.r.43mO', 'ROLE_USER', TRUE),
 (3, 'omarderouiche', '$2a$04$6B5YIsWvW4CkdVgE/9IIyONxabx4mwjmqmJhWZQr0/nWz2.r.43mO', 'ROLE_USER', TRUE),
-(4, 'admin', '$2a$04$6B5YIsWvW4CkdVgE/9IIyONxabx4mwjmqmJhWZQr0/nWz2.r.43mO', 'ROLE_USER', TRUE);
+(4, 'admin', '$2a$04$6B5YIsWvW4CkdVgE/9IIyONxabx4mwjmqmJhWZQr0/nWz2.r.43mO', 'ROLE_ADMIN', TRUE);
